149ae580caae50b928f39b979379f56dd2598ef7
shailendra98k <shailendra98k@gmail.com>
2024-02-12 23:06:05 +0530

    Integrated MS Clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,6 +33,16 @@ export default function RootLayout({ children }) {
           gtag('config', "${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}");
         `}
       </Script>
+      <Script id="ms-clarity" strategy="lazyOnload">
+        {`
+        
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY}");
+        `}
+      </Script>
       <Suspense
         fallback={
           <div style={{ postion: "absolute", top: "300px" }}>Loading...</div>
